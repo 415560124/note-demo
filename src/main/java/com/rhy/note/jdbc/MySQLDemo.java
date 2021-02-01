ffb3b8a436b014e46221963f650346730a23ba3a
Rhy <415560124@qq.com>
2021-02-01 15:52:11 +0800

    1.jdbc代码示例

---
+++ src/main/java/com/rhy/note/jdbc/MySQLDemo.java
@@ -0,0 +1,79 @@
+package com.rhy.note.jdbc;
+
+import java.sql.*;
+
+/**
+ * @author: Herion Lemon
+ * @date: 2021年02月01日 15:46:00
+ * @slogan: 如果你想攀登高峰，切莫把彩虹当梯子
+ * @description:
+ */
+public class MySQLDemo {
+    // MySQL 8.0 以下版本 - JDBC 驱动名及数据库 URL
+//    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+//    static final String DB_URL = "jdbc:mysql://localhost:3306/RUNOOB";
+
+    // MySQL 8.0 以上版本 - JDBC 驱动名及数据库 URL
+    static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://localhost:3306/cloud_rhy_member?useUnicode=true&characterEncoding=utf-8&allowMultiQueries=true&useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+
+
+    // 数据库的用户名与密码，需要根据自己的设置
+    static final String USER = "root";
+    static final String PASS = "root";
+
+    public static void main(String[] args) {
+        Connection conn = null;
+        Statement stmt = null;
+        try{
+            // 注册 JDBC 驱动
+            Class.forName(JDBC_DRIVER);
+
+            // 打开链接(数据库连接池)
+            System.out.println("连接数据库...");
+            conn = DriverManager.getConnection(DB_URL,USER,PASS);
+
+            // 执行查询(类比mybatis底层做的)
+            System.out.println(" 实例化Statement对象...");
+            stmt = conn.createStatement();
+            String sql;
+//            sql = "SELECT id, name, url FROM websites";
+            sql = "SELECT mem_id, mem_name FROM member_info";
+            ResultSet rs = stmt.executeQuery(sql);
+
+            // 展开结果集数据库
+            while(rs.next()){
+                // 通过字段检索
+                int id  = rs.getInt("mem_id");
+                String name = rs.getString("mem_name");
+
+                // 输出数据
+                System.out.print("ID: " + id);
+                System.out.print(", 站点名称: " + name);
+                System.out.print("\n");
+            }
+            // 完成后关闭
+            rs.close();
+            stmt.close();
+            conn.close();
+        }catch(SQLException se){
+            // 处理 JDBC 错误
+            se.printStackTrace();
+        }catch(Exception e){
+            // 处理 Class.forName 错误
+            e.printStackTrace();
+        }finally{
+            // 关闭资源
+            try{
+                if(stmt!=null) stmt.close();
+            }catch(SQLException se2){
+            }// 什么都不做
+            try{
+                if(conn!=null) conn.close();
+            }catch(SQLException se){
+                se.printStackTrace();
+            }
+        }
+        System.out.println("Goodbye!");
+    }
+}
