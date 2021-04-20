2a2727de1498ae81bd31b2adda54ba0585a222f5
rhy <415560124@qq.com>
2021-04-20 22:09:07 +0800

    1.

--- src/main/java/com/rhy/note/NoteDemoApplication.java
+++ src/main/java/com/rhy/note/NoteDemoApplication.java
@@ -4,7 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NoteDemoApplication {
+public class NoteDemoApplication extends Thread {
+    @Override
+    public void run() {
+        Object
+        super.run();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(NoteDemoApplication.class, args);
